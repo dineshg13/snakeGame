77008b2ebde096b6a0c6bdbaa88be2ecb0b813c4
jbreckmckye <jimmy@breck-mckye.com>
2014-04-07 23:04:27 +0100

    Readme, finish gfx engine, create demo page
    Added readme describing goals of project
    Reach first milestone for gfx engine (full interface + works, but needs
    some optimization)
    Demo page showing usage on a 404 page

--- readme.md
+++ readme.md
@@ -1,3 +1,37 @@
 ##snakeGame
 
-It's a game of Snake written in JavaScript. It's still WIP.
+A JavaScript / jQuery implementation of the classic game, *Snake*.
+
+##Wot, no canvas?
+
+No, and no SVG / VML either. Snake can be rendered as a simple grid of square cells with solid colours, so I figured I could just use the DOM. Besides, it seemed like a good way to learn about high-performance DOM manipulation and jQuery optimization.
+
+##Remind me of the rules
+
+Here's the basic tennets of Snake:
+
+- your snake is always moving. You can change its direction, but you cannot stop it
+- directionality 'cascades' down the snake (e.g. hitting left then right will make the snake's neck, body and tail weave in succession)
+- your snake cannot crash into the walls
+- your snake cannot crash into itself
+- your snake can eat apples randomly spawned to increase the player's score. Every apple eaten increases the length of the snake.
+- you win if you manage to fill the whole canvas with snake
+
+##Features (planned)
+
+- use both keyboard and touch-friendly arrow controls to move the snake
+- use localstorage to keep the player's high score
+
+##Performance aims
+I'd like snakeGame to animate at 60fps on a first generation iPad. This shouldn't be too hard, but I'd also like to try out some performance optimization for its own sake:
+
+- snakeGame doesn't add or remove DOM elements, just hides / unhides them. GC events should be minimal.
+- DOM queries and lookups to native DOM interfaces should be aliased wherever feasible
+- paints should be optimized
+
+##Design aims
+
+- separate model of game from view and game canvas rendering
+- be able to swap canvas out easily
+- game should be pretty customizable
+- try and keep final JS quite lightweight
--- snake.js
+++ snake.js
@@ -1,95 +1,238 @@
 // CONSTS
 
-var SHAPE = {
+var SHAPES = {
     SNAKE : "snake",
-    APPLE : "apple"
+    APPLE : "apple",
+    BORDER : 'border'
 };
 
-var KEY = {
-    UP : 1,
-    DOWN : 2,
-    LEFT : 3,
-    RIGHT : 4
+var DIRECTION = {
+    UP : 38,
+    DOWN : 40,
+    LEFT : 37,
+    RIGHT : 39
 };
+
+
+var TEXTURE = {
+    snake : {
+        'background-color' : '#009200',
+        'border-radius' : 0
+    },
+    apple : {
+        'background-color' : '#920000',
+        'border-radius' : '50%'
+    },
+    text : {
+        'background-color' : '#cf4000',
+        'border-radius' : 0
+    }
+};
+
 
 function Game() {
     // todo    
 }
 
-function Canvas(domTarget, width, height) {
+function Canvas(domTarget, gridX, gridY) {
     // Creates canvas in dom element
 
-    var matrix = new Matrix($(domTarget), width, height);
+    var matrix = new Matrix($(domTarget), gridX, gridY);
     
-    this.draw = function(x, y, shape) {
-        matrix.getCell(x,y).render(shape);  
-    }
+    this.draw = function(x,y, shape) {
+        var cell = matrix.getCell(x,y)
+        if (cell !== null) {
+            cell.queuedShape = shape;
+        }       
+    };
     
     this.clear = function(x,y) {
         matrix.getCell(x,y).hide();
-    }
-    
-    this.freeze = function() {
-        matrix.turnRed();
-    }
+    };
     
     this.clearAll = function() {
-        matrix.hideAll();
-    }    
+        matrix.clearAll();
+    };
+    
+    this.update = function() {
+        matrix.renderAll();
+    };
+    
+    // this.drawStraight = function(horizontal, start, end, plane, shape) {
+        // if (end < start) {
+            // var holdStart = start;
+            // start = end;
+            // end = holdStart;
+        // }
+//         
+        // var units = (end - start) +1;
+//         
+        // for (var i = 0; i < units; i++) {
+            // if (horizontal) {
+                // var xToDraw = start + i;
+                // this.draw(xToDraw, plane, shape);
+            // }
+            // else {
+                // var yToDraw = start + i;
+                // this.draw(plane, yToDraw, shape);
+            // }
+        // }
+    // };
+     
+    this.drawStraight = function(startX, startY, direction, delta, shape) {
+        var xIncrementPerPoint = 0;
+        var yIncrementPerPoint = 0;
+        
+        if (direction == DIRECTION.RIGHT) {
+            xIncrementPerPoint = 1;
+        }
+        else if (direction == DIRECTION.LEFT) {
+            xIncrementPerPoint = -1;
+        }
+        else if (direction == DIRECTION.DOWN) {
+            yIncrementPerPoint = 1;
+        }
+        else {
+            yIncrementPerPoint = -1;
+        }
+        
+        for(var i = 0; i < delta; i++) {
+            var xToDraw = startX + (i * xIncrementPerPoint);
+            var yToDraw = startY + (i * yIncrementPerPoint);
+            this.draw(xToDraw, yToDraw, shape);            
+        }
+        
+        // Make chainable
+        return {
+            x : xToDraw,
+            y : yToDraw
+        };
+    };
+    
+    this.drawPaths = function(startX, startY, commands, shape) {
+        // Commands is a tuple of [DIRECTION, DELTA]s, eg ['DOWN', 14]
+        var currentX = startX;
+        var currentY = startY;
+        
+        for (var i = 0; i < commands.length; i++) {
+            var direction = commands[i][0];
+            var delta = commands[i][1];
+            var newPosition = this.drawStraight(currentX, currentY, direction, delta, shape);
+            currentX = newPosition.x;
+            currentY = newPosition.y;
+        }
+        
+        // Make chainable
+        return {
+            x : currentX,
+            y : currentY
+        }
+        
+    }
+
 }
 
-function Matrix($domTarget, width, height) {
-    
-    function Cell(x, y, cellSize) {
-        
-        var hidden = true;
-        var cellShape = SHAPES.SNAKE;
-  
-        var cell = $('<div>');
-        $domTarget.append(cell);
-        cell.css({
-            position    : 'absolute',
-            height      : cellSize.height,
-            width       : cellSize.width,
-            left        : (x * cellSize.width),
-            top         : (y * cellSize.height)
-        });
-        
-        this.hide = function() {
-            if (hidden == false) {
-                cell.css({display : 'none'});
-                hidden = true;
-            }
-        }
-        
-        this.render = function(shape) {
-            if (cellShape == shape) {
-                // nop
-            }
-            else if (shape = SHAPES.SNAKE) {
-                // nop
-            }
-            else if (shape = SHAPES.APPLE) {
-                // nop
-            }
-        }
-        
-              
-    
-    }
+function Matrix($domTarget, gridX, gridY) {  
     
     var cellSize = {
-        width : ($domTarget.width() / width),
-        height : ($domTarget.height() / height)
+        width : ($domTarget.width() / gridX),
+        height : ($domTarget.height() / gridY)
     }
     
+    // Create matrix and add cells to dom
+    
+    $domTarget.css(Cell.containerStyles);
+    
     var rows = [];
-    for (var y = 0; y < height; y++) {
-        row[y] = [];
-        for (var x = 0; x < width; x++) {             
-            row[y][x] = new Cell(x, y, cellSize);            
+    for (var y = 0; y < gridY; y++) {
+        rows[y] = [];
+        for (var x = 0; x < gridX; x++) {             
+            rows[y][x] = new Cell(x, y, cellSize, $domTarget);            
         }        
     }
     
-    return rows;    
+    // Maxes to make map loops easier to read
+    
+    var maxRow = gridY - 1;
+    var maxCol = gridX - 1;
+    
+    this.clearAll = function(method) {
+       for (var i = 0; i <= gridY; i++) {
+            for (var j = 0; j <= gridX; j++) {
+                rows[i][j].hide();
+            }
+        }  
+    };
+    
+    this.renderAll = function(method) {
+       for (var i = 0; i <= maxRow; i++) {
+            for (var j = 0; j <= maxCol; j++) {
+                rows[i][j].render();
+            }
+        }  
+    };    
+    
+    this.getCell = function(x,y) {
+        var cell = rows[y][x];
+        if (cell) {
+            return cell
+        }
+        else {
+            return null;
+        }       
+    };
+}
+
+function Cell(x, y, cellSize, $domTarget) {
+    
+    // Initial state
+    
+    var hidden = true;
+    var cellShape = undefined;
+    this.queuedShape = undefined;
+  
+    // Add to dom
+      
+    var cell = $('<div>');
+    $domTarget.append(cell);
+    cell.css({
+        position    : 'absolute',
+        height      : cellSize.height,
+        width       : cellSize.width,
+        left        : (x * cellSize.width),
+        top         : (y * cellSize.height)
+    });
+    
+    // Interfaces
+    
+    this.hide = function() {
+        if (hidden == false) {
+            cell.css({display : 'none'});
+            hidden = true;
+        }
+    }
+    
+    this.show = function() {
+        if (hidden == true) {
+            cell.css({display : 'block'});
+            hidden = false;
+        }  
+    };
+    
+    this.render = function() {
+        if (cellShape != this.queuedShape) {
+            cell.css(TEXTURE[this.queuedShape]);
+            cellShape = this.queuedShape;
+        }
+        this.show();
+    };
+    
+    this.destroy = function() {
+        cell.remove();
+        return [x,y];
+    };        
+}
+
+Cell.containerStyles = {
+    position : 'relative'
 }
